e1fb5fced871d933da67f549b0e332e6cd14b1d0
Arnaud Carré <arnaud.carre@gmail.com>
2024-10-21 00:12:55 +0200

    ym2149 32 steps enveloppe

--- SndhArchivePlayer/SndhArchivePlayer.cpp
+++ SndhArchivePlayer/SndhArchivePlayer.cpp
@@ -422,7 +422,7 @@ void	SndhArchivePlayer::UpdateImGui()
 
 		if (ImGui::BeginPopupModal("About", NULL, ImGuiWindowFlags_AlwaysAutoResize))
 		{
-			DrawTextCentered("SNDH Archive Player v0.76");
+			DrawTextCentered("SNDH Archive Player v0.77");
 			ImGui::Separator();
 			extern void OsOpenInShell(const char* path);
 
